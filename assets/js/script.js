a8b81f2b7e762cbb6e93b400b5e2311b6736902b
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-01-29 13:15:26 -0800

    a table of timeblocks skeleton, Hours properly displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-// Display Current Time at the Header Jumbotron
+/************************************** Display Current Time at the Header Jumbotron **************************************/
+const PAST_HOURS = 3; // Display Three previous hours
+const FUTURE_HOURS = 6; // Display Six future hours
+
+
+
+/************************************** Display Current Time at the Header Jumbotron **************************************/
 // Initial display
 let currentDay = $("#currentDay");
 currentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
@@ -6,3 +12,27 @@ currentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 let headerTimer = setInterval(function() {
   currentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 }, 1000);
+
+/**************************************************** Build Time block ****************************************************/
+for(let i = -PAST_HOURS; i <= FUTURE_HOURS; i++) {
+  let timeBlock = $("<div>").addClass("row");
+  timeBlock.attr("id", "time-table");
+  timeBlock.attr("data-hour-index", i);
+  $("#time-block").append(timeBlock);
+
+  let timeBlockHour = $("<div>").addClass("col-sm-1 border border-dark");
+  timeBlockHour.text(moment().add(i, 'hours').format('h:[00] a'));
+  
+  let timeBlockContent = $("<div>").addClass("col-sm-10 border border-primary");
+  timeBlockContent.text("bibidibabidibu");
+  
+  let timeBlockSave = $("<div>").addClass("col-sm-1 border border-danger");
+  timeBlockSave.text("save");
+  
+  $("#time-table").append(timeBlockHour);
+  $("#time-table").append(timeBlockContent);
+  $("#time-table").append(timeBlockSave);
+}
+
+
+
