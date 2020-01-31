7fe35692e9d0363a61d6533dac85e18a1b5bcad4
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-01-30 18:32:11 -0800

    buildPlanner() from test data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,45 @@
-/************************************** Display Current Time at the Header Jumbotron **************************************/
-const PAST_HOURS = 3; // Display Three previous hours
-const FUTURE_HOURS = 6; // Display Six future hours
+$(document).ready(function() {
+  /************************************************ CONFIGURATION CONSTANTS ************************************************/
+  const PAST_HOURS = 3; // Display Three previous hours
+  const FUTURE_HOURS = 6; // Display Six future hours
 
-
-
-/************************************** Display Current Time at the Header Jumbotron **************************************/
-// Initial display
-let currentDay = $("#currentDay");
-currentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-// Refresh every second
-let headerTimer = setInterval(function() {
-  currentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-}, 1000);
-
-/**************************************************** Build Time block ****************************************************/
-for(let i = -PAST_HOURS; i <= FUTURE_HOURS; i++) {
-  let timeBlock = $("<div>").addClass("input-group");
-  timeBlock.attr("id", `time-table-${i}`);
-  $("#time-block").append(timeBlock);
-
-  let timeBlockHour = $("<div>").addClass("input-group-prepend");
-  let timeBlockHourButton = $("<button>").addClass("btn btn-outline-secondary disabled");
-  timeBlockHourButton.text(moment().add(i, 'hours').format('hhA'));
-  timeBlockHour.append(timeBlockHourButton);
+  /************************************************ LOCAL STORAGE FUNCTIONS ************************************************/
   
-  let timeBlockContent = $("<input>").addClass("form-control");
-  timeBlockContent.text("bibidibabidibu");
-  
-  let timeBlockSave = $("<div>").addClass("input-group-append");
-  let timeBlockSaveButton = $("<button>").addClass("btn btn-outline-primary");
-  timeBlockSaveButton.attr("data-hour-index", i);
-  timeBlockSaveButton.text("save");
-  timeBlockSave.append(timeBlockSaveButton);
-  
-  $(`#time-table-${i}`).append(timeBlockHour);
-  $(`#time-table-${i}`).append(timeBlockContent);
-  $(`#time-table-${i}`).append(timeBlockSave);
-}
+  // If data exists on local Storage
+  let calendarData = localStorage.getItem("calendarData");
+  calendarData = {};
+  calendarData["0130"] = [ "what", "intheworld", "", "", "","","","","",""];
+  if( calendarData != null ) {
+    let today = moment().format("MMDD");
+    buildCalendar(calendarData[today]);
+  }
+  else {
+    calendarData = {};
+    console.log("data e???");
+  }
 
-/**************************************************** Build Time block ****************************************************/
+  /************************************** Display Current Time at the Header Jumbotron **************************************/
+  // Initial display
+  let currentDay = $("#currentDay");
+  currentDay.text(moment().format("MMMM Do YYYY, h:mm:ss A"));
+  // Refresh every second
+  let headerTimer = setInterval(function() {
+    currentDay.text(moment().format("MMMM Do YYYY, h:mm:ss A"));
+  }, 1000);
 
+  /************************************************* Render Data on timeblock ***************************************************/
+  function buildCalendar(schedule) {
+    for (let i = 0; i < 10; i++) {
+      $(`#content-${i}`).text(schedule[i]);
+    }
+  }
+
+  /*************************************************** Save Button Function *****************************************************/
+  $(".save-button").on("click", function(event) {
+    event.preventDefault();
+    console.log($(this).attr("data-time-stamp"));
+    // Search through the data base, look for time stamp
+
+    // Modify the content on the data base
+  });
+});
